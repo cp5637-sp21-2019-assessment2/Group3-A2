c8d76a1047c5e47f74c2006fe37efd5c67024577
sirishvardhan <38369603+sirishvardhan@users.noreply.github.com>
2019-05-17 00:41:22 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+/**
+
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Group 3
+ * @since 1.0.0
+ */
 
 get_header();
 ?>
