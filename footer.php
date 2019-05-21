485e7de9222706ded6d5456260381b17b40dd0a9
aslesha948 <38353215+aslesha948@users.noreply.github.com>
2019-05-22 01:31:44 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,25 @@
-<footer>
-	This is my footer
-</footer>
+  <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          
+         <?php if(is_active_sidebar('sidebar')): ?>
+         <?php dynamic_sidebar('sidebar'); ?>
+       <?php endif; ?>
+
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+<footer class="blog-footer">
+      <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?> </p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+    <?php wp_footer(); ?>
 
 
-</body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+  </body>
 </html>
