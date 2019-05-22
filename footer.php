e7365bfa01c308f2704175a6c84c3e5432626751
aslesha948 <38353215+aslesha948@users.noreply.github.com>
2019-05-22 11:59:51 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<?php if(!is_front_page()) : ?>
   <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
           
          <?php if(is_active_sidebar('sidebar')): ?>
@@ -5,6 +6,7 @@
        <?php endif; ?>
 
         </div><!-- /.blog-sidebar -->
+        <?php endif; ?>
 
       </div><!-- /.row -->
 
